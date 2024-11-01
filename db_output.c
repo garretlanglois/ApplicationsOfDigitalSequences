614ca1496a8a74cde9d63cc9871e25a318b80f83
Garret Langlois <gaslanglois@gmail.com>
2024-11-01 17:32:47 -0400

    Create db_output.c

---
+++ db_output.c
@@ -0,0 +1,14 @@
+//Code for emulating the output of a De Bruijn Sequence
+
+#include <stdlib.h>
+#include <stdio.h>
+
+void generateSequence(int minimumPeriod, int ) {
+
+}
+
+int main(void) {
+
+    int
+
+}
